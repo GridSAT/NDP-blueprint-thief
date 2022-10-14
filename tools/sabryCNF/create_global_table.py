a7cd71bb4cddbce0f8e029b297545ba24260a9aa
Karim <78545604+alephc1@users.noreply.github.com>
2022-10-14 09:21:29 +0200

    Delete tools directory

--- tools/sabryCNF/create_global_table.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), os.pardir))
-#sys.path.append('..')
-import DbAdaptor
-
-table_name = "GlobalSetsTable_lo"
-db_adaptor = DbAdaptor.DbAdapter()
-db_adaptor.gs_create_table(table_name)
